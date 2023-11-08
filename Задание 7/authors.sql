5e6bf0ce99b446298a5711fe81f3699ff48f5d0e
Fenrir7321 <prime1010101@mail.ru>
2023-11-08 15:21:50 +0300

    Fenfefd

---
+++ Задание 7/authors.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Authors (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(255) NOT NULL,
+    birth_date DATE,
+    death_date DATE
+);
+
+INSERT INTO Authors (full_name, birth_date, death_date) VALUES
+    ('Лев Николаевич Толстой', '1828-09-09', '1910-11-20'),
+    ('Джордж Оруэлл', '1903-06-25', '1950-01-21'),
+    ('Иван Сергеевич Тургенев', '1818-11-09', '1883-09-03');
---
+++ Задание 7/books.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    page_count INT
+);
+
+INSERT INTO Books (title, author, page_count) VALUES
+    ('Война и мир', 'Лев Николаевич Толстой', 1300),
+    ('1984', 'Джордж Оруэлл', 320),
+    ('Муму', 'Иван Сергеевич Тургенев', 124);
